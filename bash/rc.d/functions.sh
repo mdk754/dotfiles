a83cd7fa73d65deb2a8cca16c35fd359d85c5a2f
Brian <9956583+mdk754@users.noreply.github.com>
2018-11-12 10:12:51 -0500

    Add function to create gzipped tar of directory

---
+++ bash/rc.d/functions.sh
@@ -0,0 +1,37 @@
+# Check if a command exists.
+command-exists() {
+	command -v "$1" 2>&1 > /dev/null
+	return $?
+}
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+tgz() {
+	local __df_file="${@%/}.tar"
+
+	printf "Creating ${__df_file} … "
+	tar -cf "${__df_file}" "${@}" || { printf "✖\n" && return 1; }
+	printf "✔\n"
+
+	local __df_size=$(stat -c"%s" "${__df_file}" 2> /dev/null)
+	local __df_command=""
+
+	if (( __df_size < 52428800 )) && command-exists zopfli; then
+		__df_command="zopfli"
+	else
+		if command-exists pigz; then
+			__df_command="pigz"
+		else
+			__df_command="gzip"
+		fi
+	fi
+
+	printf "Compressing ${__df_file} ($((__df_size / 1024)) kB) using \`${__df_command}\` … "
+	"${__df_command}" "${__df_file}" || { printf "✖\n" && return 1; }
+	printf "✔\n"
+
+	[ -f "${__df_file}" ] && rm "${__df_file}"
+
+	__df_size=$(stat -c"%s" "${__df_file}.gz" 2> /dev/null)
+
+	printf "\n${__df_file}.gz ($((__df_size / 1024)) kB) created successfully.\n"
+}
