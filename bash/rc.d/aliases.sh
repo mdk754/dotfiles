11d03ba17dfffca508d2869da1a0a7351aaa7089
Brian <9956583+mdk754@users.noreply.github.com>
2018-11-11 22:19:45 -0500

    Add aliases for working with git

--- bash/rc.d/aliases.sh
+++ bash/rc.d/aliases.sh
@@ -25,3 +25,13 @@ alias reload='exec ${SHELL}'
 
 # Print each PATH entry on a separate line
 alias path='printf ${PATH//:/\\n}'
+
+# Convenient git shortcuts.
+alias gf='git fetch --prune --all'
+alias gcr='git commit -m "(needs rebase)"'
+alias gcm='git commit -m'
+alias glo='git log --oneline --decorate'
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit'
+alias glola='git log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
